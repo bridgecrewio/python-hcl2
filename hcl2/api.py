5fce3f2fb1e8f0b2c666bb777032e937096be1d5
gruebel <anton.gruebel@gmail.com>
2022-04-06 18:31:44 +0200

    fix lint issue

--- hcl2/api.py
+++ hcl2/api.py
@@ -59,11 +59,19 @@ def loads(text: str) -> dict[str, list[dict[str, Any]]]:
             # (but we don't want to do this for every line if it's not necessary)
             stripped = strip_line_comment(line)[0] if not comment else line  # maybe we already stripped it
             if stripped.replace('\\"', '').count('"') % 2 != 0:
-                if "${" in stripped and stripped.index('"') < stripped.index("${"):
+                if (
+                    "${" in stripped
+                    and "}" not in stripped
+                    and stripped.index('"') < stripped.index("${")
+                ):
                     # this seems to be a multiline interpolation string
                     found_multi_line_interpolation_start = True
                     continue
-                if found_multi_line_interpolation_start and "}" in stripped and stripped.index("}") < stripped.index('"'):
+                if (
+                    found_multi_line_interpolation_start
+                    and "}" in stripped
+                    and stripped.index("}") < stripped.index('"')
+                ):
                     # found also the closing end
                     found_multi_line_interpolation_start = False
                     continue
