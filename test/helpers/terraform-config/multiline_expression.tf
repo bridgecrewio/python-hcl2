0ef6ac1b908d570ba641796363e605d21addb513
Anton Grübel <anton.gruebel@gmail.com>
2022-05-15 12:00:38 +0200

    support empty array/tuple (#30)

--- test/helpers/terraform-config/multiline_expression.tf
+++ test/helpers/terraform-config/multiline_expression.tf
@@ -77,3 +77,26 @@ variable "some_var2" {
     # comment 8
   )
 }
+
+resource "null_resource" "multiline_comment_multiline" {
+    triggers = [
+        /* "a",
+        "b" */
+    ]
+}
+
+resource "null_resource" "multiline_comment_single_line_before_closing_bracket" {
+    triggers = [
+        /* "a", "b" */ ]
+}
+
+resource "null_resource" "multiline_comment_single_line_between_brackets" {
+    triggers = [
+        /* "a", "b" */
+    ]
+}
+
+resource "null_resource" "multiline_comment_single_line_after_opening_bracket" {
+    triggers = [ /* "a", "b" */
+    ]
+}
